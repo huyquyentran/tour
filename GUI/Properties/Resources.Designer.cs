e279d3721cc2f0e8aa898e0f2bf291296a7ebf73
HoangTran <99.hoangtran@gmail.com>
2020-11-17 15:54:27 +0700

    add icon Resources

--- GUI/Properties/Resources.Designer.cs
+++ GUI/Properties/Resources.Designer.cs
@@ -63,6 +63,46 @@ namespace GUI.Properties {
         /// <summary>
         ///   Looks up a localized resource of type System.Drawing.Bitmap.
         /// </summary>
+        internal static System.Drawing.Bitmap icons8_adventure_50px {
+            get {
+                object obj = ResourceManager.GetObject("icons8_adventure_50px", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized resource of type System.Drawing.Bitmap.
+        /// </summary>
+        internal static System.Drawing.Bitmap icons8_baby_footprint_50px {
+            get {
+                object obj = ResourceManager.GetObject("icons8_baby_footprint_50px", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized resource of type System.Drawing.Bitmap.
+        /// </summary>
+        internal static System.Drawing.Bitmap icons8_management_50px {
+            get {
+                object obj = ResourceManager.GetObject("icons8_management_50px", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized resource of type System.Drawing.Bitmap.
+        /// </summary>
+        internal static System.Drawing.Bitmap icons8_user_groups_50px {
+            get {
+                object obj = ResourceManager.GetObject("icons8_user_groups_50px", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized resource of type System.Drawing.Bitmap.
+        /// </summary>
         internal static System.Drawing.Bitmap wine_tour_logo {
             get {
                 object obj = ResourceManager.GetObject("wine-tour-logo", resourceCulture);

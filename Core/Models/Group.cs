4f57c7511b08dd949a47290a6bcd66f08ebce63d
Huy Quyen <huyquyent@gmail.com>
2020-11-19 02:35:33 +0700

    [Feature] List, Add, Update, Romove Group, List customer

--- Core/Models/Group.cs
+++ Core/Models/Group.cs
@@ -6,56 +6,20 @@ namespace Core.Models
 {
     public class Group
     {
-        public Group () { }
-        public Group(DateTime startDate, DateTime endDate, int tourId)
+        public Group() { }
+        public Group(string name, DateTime startDate, DateTime endDate, int tourId)
         {
+            Name = name;
             StartDate = startDate;
             EndDate = endDate;
             TourId = tourId;
         }
         public int Id { get; set; }
+        public string Name { get; set; }
         public DateTime StartDate { get; set; }
         public DateTime EndDate { get; set; }
         public int TourId { get; set; }
         public Tour Tour { get; set; }
-        public int PriceTour {
-            get
-            {
-                if (Tour == null) return 0;
-                var tourPrices = Tour.TourPrices;
-                foreach(var tourPrice in tourPrices)
-                {
-                    if (new DateRange(tourPrice.StartDate, tourPrice.EndDate).Includes(StartDate))
-                    {
-                        return tourPrice.Price;
-                    }    
-                }
-                return Tour.CurrentPrice;
-            }
-            set {;} 
-        }
-        public int PriceCosts
-        {
-            get
-            {
-                int temp = 0;
-                if (Costs == null) return temp;
-                foreach(var cost in Costs)
-                {
-                    temp += cost.Price;
-                }
-                return temp;
-            }
-            set {;}
-        }
-        public int TotalPrice
-        {
-            get
-            {
-                return PriceCosts + PriceTour;
-            }
-            set {; }
-        }
         public ICollection<CustomerGroups> CustomerGroups { get; set; }
         public ICollection<Assignment> Assignments { get; set; }
         public ICollection<Cost> Costs { get; set; }
---
+++ DAL/Migrations/202011181503462_ChangeTableGroup.cs
@@ -0,0 +1,24 @@
+﻿namespace Core.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class ChangeTableGroup : DbMigration
+    {
+        public override void Up()
+        {
+            AddColumn("dbo.Groups", "Name", c => c.String());
+            DropColumn("dbo.Groups", "PriceTour");
+            DropColumn("dbo.Groups", "PriceCosts");
+            DropColumn("dbo.Groups", "TotalPrice");
+        }
+        
+        public override void Down()
+        {
+            AddColumn("dbo.Groups", "TotalPrice", c => c.Int(nullable: false));
+            AddColumn("dbo.Groups", "PriceCosts", c => c.Int(nullable: false));
+            AddColumn("dbo.Groups", "PriceTour", c => c.Int(nullable: false));
+            DropColumn("dbo.Groups", "Name");
+        }
+    }
+}
